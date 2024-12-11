92b87652ac60dfca8a4f3526d7fb356373df84cc
KevinC <xchen935@wisc.edu>
2024-12-11 10:39:01 +0800

    navigation bar

--- src/views/Home/components/home-channel.vue
+++ src/views/Home/components/home-channel.vue
@@ -1,20 +1,29 @@
 <template>
   <van-tabs v-model:active="active">
-    <van-tab title="tab 1" name="a">content of tab 1</van-tab>
-    <van-tab title="tab 2" name="b">content of tab 2</van-tab>
-    <van-tab title="tab 3" name="c">content of tab 3</van-tab>
-    <van-tab title="tab 4" name="d">content of tab 4</van-tab>
+    <van-tab v-for="item in list" :key="item.id" :title="item.text" name="a"></van-tab>
   </van-tabs>
 </template>
 
 <script setup lang="ts">
+// TypeScript 的接口用于标记数据格式
 import { ref } from 'vue'
 import axios from 'axios'
-const active = ref(0)
 
 interface INvaItem{
-    id: string
-    text: string
+  id: string
+  text: string
 }
+
+const active = ref(0)
+// 频道数据，TypeScript <INavItem[]> 表示list数据为数组，数组的每一项需要符合INavItem接口的格式
+// pros，模板中使用list和item的时候，鼠标移入有类型提醒
 const list = ref<INvaItem[]>([])
+
+axios({
+  url: '/navList',
+  method: 'get'
+}).then(res => {
+  console.log(res.data)
+  list.value = res.data.result
+})
 </script>
