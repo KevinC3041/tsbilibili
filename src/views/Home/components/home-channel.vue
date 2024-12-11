975163bea244ccbd357e34a9c869f10449b32588
KevinC <xchen935@wisc.edu>
2024-12-11 09:28:32 +0800

    basic page structure

---
+++ src/views/Home/components/home-channel.vue
@@ -0,0 +1,20 @@
+<template>
+  <van-tabs v-model:active="active">
+    <van-tab title="tab 1" name="a">content of tab 1</van-tab>
+    <van-tab title="tab 2" name="b">content of tab 2</van-tab>
+    <van-tab title="tab 3" name="c">content of tab 3</van-tab>
+    <van-tab title="tab 4" name="d">content of tab 4</van-tab>
+  </van-tabs>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+const active = ref(0)
+
+interface INvaItem{
+    id: string
+    text: string
+}
+const list = ref<INvaItem[]>([])
+</script>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,8 +1,14 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+    <!-- 头部组件 -->
     <AppHeader />
+    <!-- 频道组件 -->
+    <HomeChannel />
+
 </template>
 
 <script setup lang="ts">
+// script setup 只需导入组件，无需注册
 import AppHeader from '@/components/app-header.vue'
+import HomeChannel from './components/home-channel.vue'
 </script>
